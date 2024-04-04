a3761b1bcbb272aaefc56678208e7985432f0277
hiroshi nagao <hi_1205@hotmail.com>
2024-04-04 16:21:20 +0900

    add solution for 8kyu

--- 8kyu/simple_multiplicatoin.rb
+++ 8kyu/simple_multiplicatoin.rb
@@ -3,12 +3,12 @@
 
 def simple_multiplication(number)
   if number % 2 == 0
-     number * 8
-   else
+    number * 8
+  else
     number * 9
-   end
+  end
  end
 
  def simple_multiplication(number)
-  number % 2 == 0 ? number * 8 : number * 9
+  (number % 2) == 0 ? number * 8 : number * 9
  end
