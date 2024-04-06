df8a1295384ac8bf5bbeb5c7435aef7778a02b9c
hiroshi nagao <hi_1205@hotmail.com>
2024-04-06 17:16:39 +0900

    modify rock paper scissors solution

--- 8kyu/rock_paper_scissors.rb
+++ 8kyu/rock_paper_scissors.rb
@@ -10,9 +10,6 @@
 def rps(p1, p2)
   return "Draw!" if p1 == p2
 
-  if p1 == 'rock' &&  p2 == 'scissors' ||  p1 == 'scissors' &&  p2 == 'paper' || p1 == 'paper' &&  p2 == 'rock'
-    return "Player 1 won!"
-   else p2 == 'scissors' &&  p1 == 'rock' || p2 == 'paper' &&  p1 == 'scissors' || p2 == 'rock' &&  p1 == 'paper'
-    "Player 2 won!"
-   end
- end
+  p1 == 'rock' &&  p2 == 'scissors' ||  p1 == 'scissors' &&  p2 == 'paper' || p1 == 'paper' &&  p2 == 'rock' ?
+  "Player 1 won!" : "Player 2 won!"
+end
