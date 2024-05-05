76033edb45ac3fa15e16df08282a27786c63e95c
hiroshi nagao <hi_1205@hotmail.com>
2024-05-05 17:51:04 +0900

    add solution

---
+++ 8kyu/double_char.rb
@@ -0,0 +1,11 @@
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+# Examples (Input -> Output):
+# * "String"      -> "SSttrriinngg"
+# * "Hello World" -> "HHeelllloo  WWoorrlldd"
+# * "1234!_ "     -> "11223344!!__  "
+# My Solution
+
+def double_char(str)
+  str.chars.map {|element| element*2}.join
+endg
