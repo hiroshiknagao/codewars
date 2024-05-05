2408b3e7974bbf439ba5d57d4583591f111423e7
hiroshi nagao <hi_1205@hotmail.com>
2024-05-05 19:44:19 +0900

    add solution

---
+++ 6kyu/find_the_unique_number.rb
@@ -0,0 +1,12 @@
+# There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+# find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+# find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+# It’s guaranteed that array contains at least 3 numbers.
+
+# The tests contain some very huge arrays, so think about performance.
+# My solution
+
+def find_uniq(arr)
+  arr.uniq.find { |element| arr.count(element) == 1 }
+end
